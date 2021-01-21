46b15abd94f1a64e124b7c12083ee98e2cea4032
JoaoBagvanji <joaobagvanji99@gmail.com>
2021-01-21 15:45:43 +0200

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculadora msg="Calculadora Basica!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculadora from './components/Calculadora.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Calculadora
   }
 }
 </script>
